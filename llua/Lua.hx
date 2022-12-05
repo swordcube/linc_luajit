40f88d0a5dfd0c0164d01413c4f7ea5290bc7c64
swordcube <49110074+swordcube@users.noreply.github.com>
2022-12-05 03:38:33 -0600

    Update Lua.hx

--- llua/Lua.hx
+++ llua/Lua.hx
@@ -579,27 +579,10 @@ class Lua_helper {
 
 		var ret:Dynamic = null;
 
-		switch (nparams) {
-			case 0:
-				ret = cbf();
-			case 1:
-				ret = cbf(args[0]);
-			case 2:
-				ret = cbf(args[0], args[1]);
-			case 3:
-				ret = cbf(args[0], args[1], args[2]);
-			case 4:
-				ret = cbf(args[0], args[1], args[2], args[3]);
-			case 5:
-				ret = cbf(args[0], args[1], args[2], args[3], args[4]);
-			case 6:
-				ret = cbf(args[0], args[1], args[2], args[3], args[4], args[5]);
-			case 7:
-				ret = cbf(args[0], args[1], args[2], args[3], args[4], args[5], args[6]);
-			case 8:
-				ret = cbf(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7]);
-			default:
-				throw("> 5 arguments is not supported");
+		if (nparams <= 0) {
+			ret = cbf();
+		} else {
+			ret = Reflect.callMethod(null, cbf, args);
 		}
 
 		if(ret != null){
